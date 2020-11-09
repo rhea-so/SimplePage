74c9527f2cc3bd6db7e60efe707183b01436c1ce
rhea-so <rhea31@naver.com>
2020-11-09 16:48:36 +0900

    요청시, 보여주는 웹페이지에서 아이피도 보여주게끔 수정함

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
+const ip = require('ip');
 const express = require('express');
 const app = express();
 
 app.get('/', (_req, res) => {
-  res.send('Hello, World!');
+  console.log(new Date(), 'Received request');
+  res.send('Hello, World!<br />This Service IP: ' + ip.address());
 });
 
 app.listen(8080, () => console.log('SimplePage is now online on port 8080'));
